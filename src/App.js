00c874b294b1f11b5eb5155faf1f3369a9623691
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-16 09:28:43 +0200

    Added router and displayed home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import GlobalStyle from "./Global";
+import Header from "./layouts/Header";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Page404 from "./components/Page404";
 
 function App() {
   return (
-    <div>
-      <h1>CVMS</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={Page404}/>
+      </Switch>
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
