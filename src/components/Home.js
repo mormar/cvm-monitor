dcc7dbbcefcae095ca812cbcd02dea6889ab4e66
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-16 09:27:09 +0200

    Added basic home page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Home = () => {
+  return (
+    <Main>
+      <h1>Home</h1>
+    </Main>
+  );
+};
+
+export default Home;
