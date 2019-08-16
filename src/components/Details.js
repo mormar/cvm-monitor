bf28e32f51699dd1c0c7a55a9e7aaedd82868fb5
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-16 17:13:29 +0200

    Added basic details page

---
+++ src/components/Details.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Details = () => {
+  return (
+    <Main>
+      <h1>Details</h1>
+    </Main>
+  );
+};
+
+export default Details;
+
