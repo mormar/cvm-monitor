fce332acf4f4268aa4ae2119aa4c892a223c7185
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-16 17:25:56 +0200

    Added details page to routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./layouts/Header";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Page404 from "./components/Page404";
+import Details from "./components/Details";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path='/:repository_name' component={Details}/>
         <Route component={Page404}/>
       </Switch>
       <GlobalStyle />
