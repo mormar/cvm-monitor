f5834e78864a6cf4f4c5f07eacf7bc62caad54cf
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-18 20:20:50 +0200

    Added input element

---
+++ src/elements/Inputs.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { roboto, whiteSmoke } from "../utilities";
+
+export const Input = styled.input`
+  background-color: ${whiteSmoke};
+  border-radius: 10px;
+  font-size: 1.5em;
+  line-height: 2em;
+  font-weight: 400;
+  border: none;
+  padding: 0 10px;
+  ${roboto};
+  width: 50%;
+`;
