0f45fb566398b075b2fba8ca8f41241979490dd5
Marcin Mokrzan <markar.mormar@gmail.com>
2019-08-21 11:10:47 +0200

    Added new colors

--- src/utilities/Colors.js
+++ src/utilities/Colors.js
@@ -3,4 +3,8 @@ export const gray = "#292929";
 export const whiteSmoke = "#f8f8f8";
 export const darkGray = "#A9A9A9";
 export const black = "#000";
-// #efeeec
+export const shipGrey = "#423c49";
+export const athensGrey = "#ECEEF5";
+export const grey = "#F1F1F3"; 
+export const gravel = "#4A424A";
+export const border = "#707070";
